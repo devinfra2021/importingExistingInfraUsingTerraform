00383e154766d1746fddbd110e146a9ea141535a
devinfra2021 <freetier12aws@outlook.com>
2021-12-01 13:34:27 +0000

    modified importEC2.tf files

--- importEC2.tf
+++ importEC2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "importManualInfraEC2" {
-	ami	="ami-0108d6a82a783b352"
+	ami	="ami-#############"
 	instance_type	="t2.micro"
-	subnet_id	="subnet-0883ae44"
+	subnet_id	="subnet-#####"
 	key_name	="terraform"
-	vpc_security_group_ids	=["sg-00e74e1ea27a2c3ca"]
+	vpc_security_group_ids	=["sg-########"]
 
 	tags={
 	Name 	="terraform"
